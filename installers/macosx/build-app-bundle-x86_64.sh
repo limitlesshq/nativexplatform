c59cb62c668a6da5aee52458a5ba852da3d161f8
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2016-09-02 17:29:19 +0300

    Build scripts for Mac OS X
    Now with 32-bit and 64-bit support

--- installers/macosx/build-app-bundle-x86_64.sh
+++ installers/macosx/build-app-bundle-x86_64.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-svn export ../../output/extract.app extract.app
+cp -Rvf ../../output/extract.app extract.app
 
 pushd ../../source/extract/
-lazbuild -B --os=darwin --ws=carbon --cpu=i386 extract.lpr
+lazbuild -B --os=darwin --ws=cocoa --cpu=x86_64 extract.lpr
 popd
 strip ../../output/extract
+rm -f extract.app/Contents/MacOS/extract
 cp ../../output/extract extract.app/Contents/MacOS/extract
 cp Info.plist extract.app/Contents
 
 mv extract.app Akeeba\ eXtract\ Wizard.app
-zip -r akeeba-extract-wizard-max-os-x.zip Akeeba\ eXtract\ Wizard.app
+zip -r akeeba-extract-wizard-max-os-x-x86_64.zip Akeeba\ eXtract\ Wizard.app
 mv *.zip ../../output
 rm -rf *.app
