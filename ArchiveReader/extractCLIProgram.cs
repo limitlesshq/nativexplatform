74437f189dddd0b919c1d0df26c6bdfa7b677577
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2016-12-06 13:01:07 +0100

    Rename the program file

---
+++ ArchiveReader/extractCLIProgram.cs
@@ -0,0 +1,20 @@
+﻿using Akeeba.Unarchiver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akeeba.extractCLI
+{
+    class extractCLIProgram
+    {
+        static void Main(string[] args)
+        {
+            using (Unarchiver.Unarchiver extractor = Unarchiver.Unarchiver.createFor(@"C:\Apache24\htdocs\backups\test.jpa"))
+            {
+                // TODO Write some code
+            }
+        }
+    }
+}
