1563cf18ccb90c453b352503d38196f704daa719
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2016-12-06 12:44:37 +0100

    Finishing the JPA::extract() method
    All code is still untested

--- ArchiveReader/Unarchiver/Format/JPA.cs
+++ ArchiveReader/Unarchiver/Format/JPA.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Akeeba.Unarchiver.DataWriter;
+using System.Threading;
+using ICSharpCode.SharpZipLib.BZip2;
 
 namespace Akeeba.Unarchiver.Format
 {
@@ -25,6 +27,7 @@ namespace Akeeba.Unarchiver.Format
             public ulong uncompressedSize;
             public ulong compressedSize;
             public ulong totalParts;
+            public ulong totalLength;
         }
 
         /// <summary>
@@ -66,9 +69,71 @@ namespace Akeeba.Unarchiver.Format
         /// Implements the extract method which extracts a backup archive. A DataWriter must be already assigned and configured or an
         /// exception will be raised.
         /// </summary>
-        public override void extract()
+        public override void extract(CancellationToken token)
         {
-            // TODO
+            // Initialize
+            close();
+            progress.filePosition = 0;
+            progress.runningCompressed = 0;
+            progress.runningUncompressed = 0;
+            progress.status = extractionStatus.running;
+            ProgressEventArgs args;
+
+            try
+            {
+                // Read the file header
+                jpaArchiveHeader archiveHeader = readArchiveHeader();
+
+                // Invoke event at start of extraction
+                args = new EventArgs.ProgressEventArgs(progress);
+                onProgressEvent(args);
+
+                while (progress.filePosition < archiveHeader.totalLength)
+                {
+                    jpaFileHeader fileHeader = readFileHeader();
+
+                    // See https://www.codeproject.com/articles/742774/cancel-a-loop-in-a-task-with-cancellationtokens
+                    if (token.IsCancellationRequested)
+                    {
+                        token.ThrowIfCancellationRequested();
+                    }
+
+                    processDataBlock(fileHeader, token);
+                }
+            }
+            catch (OperationCanceledException cancelledException)
+            {
+                // The operation was cancelled. Set the state to Idle and reset the extraction.
+                close();
+                progress.filePosition = 0;
+                progress.runningCompressed = 0;
+                progress.runningUncompressed = 0;
+                progress.status = extractionStatus.idle;
+
+                // Invoke an event notifying susbcribers about the cancelation.
+                args = new EventArgs.ProgressEventArgs(progress);
+                onProgressEvent(args);
+
+                return;
+            }
+            catch (Exception errorException)
+            {
+                // Any other exception. Close the option file and set the status to error.
+                close();
+                progress.status = extractionStatus.error;
+                progress.exception = errorException;
+
+                // Invoke an event notifying of the error state
+                args = new EventArgs.ProgressEventArgs(progress);
+                onProgressEvent(args);
+
+                return;
+            }
+
+            // Invoke an event signaling the end of extraction
+            progress.status = extractionStatus.finished;
+            args = new EventArgs.ProgressEventArgs(progress);
+            onProgressEvent(args);
         }
 
         /// <summary>
@@ -149,6 +214,8 @@ namespace Akeeba.Unarchiver.Format
                 FileInfo fi = new FileInfo(archivePath);
                 info.archiveSize += (ulong) fi.Length;
             }
+
+            archiveHeader.totalLength = info.archiveSize;
 
             // -- Incorporate bits from the file header
             info.fileCount = archiveHeader.fileCount;
@@ -242,8 +309,22 @@ namespace Akeeba.Unarchiver.Format
             return fileHeader;
         }
 
-        public void processDataBlock(jpaFileHeader dataBlockHeader)
+        /// <summary>
+        /// Processes a data block in a JPA file located in the current file position
+        /// </summary>
+        /// <param name="dataBlockHeader">The header of the block being processed</param>
+        /// <param name="token">A cancellation token, allowing the called to cancel the processing</param>
+        public void processDataBlock(jpaFileHeader dataBlockHeader, CancellationToken token)
         {
+            // Update the archive's progress record
+            progress.filePosition = (ulong)(sizesOfPartsAlreadyRead + inputFile.Position);
+            progress.runningCompressed += dataBlockHeader.compressedSize;
+            progress.runningUncompressed += dataBlockHeader.compressedSize;
+            progress.status = extractionStatus.running;
+
+            // Create the event arguments we'll use when invoking the event
+            ProgressEventArgs args = new EventArgs.ProgressEventArgs(progress);
+
             // If we don't have a data writer we just need to skip over the data
             if ((dataWriter == null) || !(dataWriter is IDataWriter))
             {
@@ -261,7 +342,8 @@ namespace Akeeba.Unarchiver.Format
                 case jpaEntityType.directory:
                     dataWriter.makeDirRecursive(dataWriter.getAbsoluteFilePath(dataBlockHeader.entityPath));
 
-                    // TODO Trigger event
+                    // Invoke the onEntityEvent event
+                    onProgressEvent(args);
 
                     return;
                     break;
@@ -273,7 +355,8 @@ namespace Akeeba.Unarchiver.Format
                         dataWriter.makeSymlink(strTarget, dataWriter.getAbsoluteFilePath(dataBlockHeader.entityPath));
                     }
 
-                    // TODO Trigger event
+                    // Invoke the onEntityEvent event
+                    onProgressEvent(args);
 
                     return;
                     break;
@@ -292,15 +375,15 @@ namespace Akeeba.Unarchiver.Format
             switch (dataBlockHeader.compressionType)
             {
                 case jpaCompressionType.uncompressed:
-                    processUncompressedDataBlock(dataBlockHeader.compressedSize);
+                    processUncompressedDataBlock(dataBlockHeader.compressedSize, token);
                     break;
 
                 case jpaCompressionType.gzip:
-                    processGZipDataBlock(dataBlockHeader.compressedSize);
+                    processGZipDataBlock(dataBlockHeader.compressedSize, token);
                     break;
 
                 case jpaCompressionType.bzip2:
-                    throw new Exception("BZip2 compression is not currently supported");
+                    processBZip2DataBlock(dataBlockHeader.compressedSize, token);
                     break;
 
             }
@@ -308,54 +391,48 @@ namespace Akeeba.Unarchiver.Format
             // Stop writing data to the file
             dataWriter.stopFile();
 
-            // Invoke the onEntityEvent event. We need to do some work to get there, through...
-            // -- Create a new archive information record
-            extractionProgress info = new extractionProgress();
-            info.filePosition = 0;
-            info.runningCompressed = 0;
-            info.runningUncompressed = 0;
-            info.status = extractionStatus.running;
-
-
-            // -- Incorporate bits from the file header
-            info.compressedSize = fileHeader.compressedSize;
-            info.uncompressedSize = fileHeader.uncompressedSize;
-            info.storedName = fileHeader.entityPath;
-            // -- Get the absolute path of the file
-            info.absoluteName = "";
-
-            if ((dataWriter != null) && (dataWriter is IDataWriter))
-            {
-                info.absoluteName = dataWriter.getAbsoluteFilePath(fileHeader.entityPath);
-            }
-            // -- Get some bits from the currently open archive file
-            info.partNumber = (int)currentPartNumber;
-            info.partOffset = inputFile.Position;
-            // -- Create the event arguments object
-            ProgressEventArgs args = new EventArgs.ProgressEventArgs(info);
-            // -- Finally, invoke the event
-            onProgressEvent(args)
-
-
+            // Invoke the onEntityEvent event
+            onProgressEvent(args);
         }
 
         /// <summary>
         /// Processes a GZip-compressed data block
         /// </summary>
         /// <param name="compressedLength">Length of the data block in bytes</param>
-        protected void processGZipDataBlock(ulong compressedLength)
+        /// <param name="token">A cancellation token, allowing the called to cancel the processing</param>
+        protected void processGZipDataBlock(ulong compressedLength, CancellationToken token)
         {
-            Stream memStream = readIntoStream((int) compressedLength);
-            GZipStream decompressStream = new GZipStream(memStream, CompressionMode.Decompress);
+            using (Stream memStream = readIntoStream((int)compressedLength))
+            {
+                using (GZipStream decompressStream = new GZipStream(memStream, CompressionMode.Decompress))
+                {
+                    dataWriter.writeData(decompressStream);
+                }
+            }
+        }
 
-            dataWriter.writeData(decompressStream);
+        /// <summary>
+        /// Processes a BZip2-compressed data block
+        /// </summary>
+        /// <param name="compressedLength">Length of the data block in bytes</param>
+        /// <param name="token">A cancellation token, allowing the called to cancel the processing</param>
+        protected void processBZip2DataBlock(ulong compressedLength, CancellationToken token)
+        {
+            using (Stream memStream = readIntoStream((int)compressedLength))
+            {
+                using (BZip2InputStream decompressStream = new BZip2InputStream(memStream))
+                {
+                    dataWriter.writeData(decompressStream);
+                }
+            }
         }
 
         /// <summary>
         /// Processes an uncompressed data block
         /// </summary>
         /// <param name="length">Length of the data block in bytes</param>
-        protected void processUncompressedDataBlock(ulong length)
+        /// <param name="token">A cancellation token, allowing the called to cancel the processing</param>
+        protected void processUncompressedDataBlock(ulong length, CancellationToken token)
         {
             // Batch size for copying data (1 Mb)
             ulong batchSize = 1048576;
@@ -366,8 +443,15 @@ namespace Akeeba.Unarchiver.Format
                 ulong nextBatch = Math.Min(length, batchSize);
                 length -= nextBatch;
 
-                Stream readData = readIntoStream((int)batchSize);
-                dataWriter.writeData(readData);
+                using (Stream readData = readIntoStream((int)batchSize))
+                {
+                    dataWriter.writeData(readData);
+                }
+
+                if (token.IsCancellationRequested)
+                {
+                    token.ThrowIfCancellationRequested();
+                }
             }
         }
     }
