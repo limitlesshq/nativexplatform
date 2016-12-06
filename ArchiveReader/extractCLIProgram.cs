9105f15207ca937b53461476cdefb188f78de634
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2016-12-06 18:07:40 +0100

    Stupidly simple test programs
    Hardcoded paths, even

--- ArchiveReader/extractCLIProgram.cs
+++ ArchiveReader/extractCLIProgram.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Akeeba.extractCLI
@@ -11,10 +12,44 @@ namespace Akeeba.extractCLI
     {
         static void Main(string[] args)
         {
-            using (Unarchiver.Unarchiver extractor = Unarchiver.Unarchiver.createFor(@"C:\Apache24\htdocs\backups\test.jpa"))
+            Unarchiver.Unarchiver extractor = null;
+
+            try
             {
-                // TODO Write some code
+                extractor = Unarchiver.Unarchiver.createFor(@"C:\Apache24\htdocs\backups\test.jpa");
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                var token = cts.Token;
+
+                Task t = Task.Factory.StartNew(
+                    () =>
+                    {
+                        extractor.scan(token);
+                        Console.WriteLine("I am done, I think");
+                    }, token,
+                    TaskCreationOptions.LongRunning,
+                    TaskScheduler.Default
+                    );
+
+                t.Wait(token);
             }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR:");
+                Console.WriteLine(e.Message);
+
+                Console.ReadLine();
+            }
+            finally
+            {
+                if ((extractor != null) && (extractor is IDisposable))
+                {
+                    extractor.Dispose();
+                }
+            }
+
+            Console.WriteLine("All done. Press ENTER");
+            Console.ReadLine();
         }
     }
 }
