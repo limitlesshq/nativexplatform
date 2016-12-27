09c6532ea68cb32454924aff711a8fcb2d300e77
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2016-12-27 13:38:05 +0200

    Version information

--- ExtractWizard/Properties/AssemblyInfo.cs
+++ ExtractWizard/Properties/AssemblyInfo.cs
@@ -1,20 +1,21 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExtractWizard")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Akeeba eXtract Wizard")]
+[assembly: AssemblyDescription("Extract backup archives made with Akeeba Backup with an easy to use interface")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ExtractWizard")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Akeeba Ltd")]
+[assembly: AssemblyProduct("Akeeba Backup Portable Tools")]
+[assembly: AssemblyCopyright("Copyright (C)2006-2016 Nicholas K. Dionysopoulos / Akeeba Ltd")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
+// Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
